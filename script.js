b04a2502c3b340163f62af92d791923997ba85e1
thiagohenrique71 <thiagospinebaster71@gmail.com>
2025-07-07 20:44:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarmensagem() {
+    document.getElementById("mensagem").innerHTML = "Para ser sávio, preciso primeiro temer a Deus, o SENHOR, Se você conhece o Deus santo, Então você tem compreensão das coisas";
+}
