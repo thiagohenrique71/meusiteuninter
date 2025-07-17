44ac139eb7144832d7598c0adf8ef18a47145d57
thiagohenrique71 <thiagospinebaster71@gmail.com>
2025-07-17 06:49:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function mostrarmensagem() {
-    document.getElementById("mensagem").innerHTML = "Para ser sávio, preciso primeiro temer a Deus, o SENHOR, Se você conhece o Deus santo, Então você tem compreensão das coisas";
+    document.getElementById("mensagem").innerHTML = "Para ser sábio, preciso primeiro temer a Deus, o SENHOR, Se você conhece o Deus santo, Então você tem compreensão das coisas";
 }
